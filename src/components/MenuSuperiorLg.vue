4a96f7bfe9515fd4ffdd397da483fecb370927ae
carolluckmann <carolinaluckmann05@gmail.com>
2024-03-18 11:09:10 -0300

    atualizacao arquivo logo.png

--- src/components/MenuSuperiorLg.vue
+++ src/components/MenuSuperiorLg.vue
@@ -11,7 +11,7 @@
 <template>
   <header>
     <div class="header--logo">
-      <img src="@/assets/logoFakeStore.png" alt="Logo" />
+      <img src="@/assets/logo.png" alt="Logo" />
       <h1>FakeStore - LG</h1>
     </div>
     <nav>
