531a18de1a25259a4fdb1e8899a78ea505f26263
carolluckmann <carolinaluckmann05@gmail.com>
2024-03-18 10:59:22 -0300

    responsividade com componentes dinamicos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <script setup>
+  import ListagemProdutos from '@/components/ListagemProdutos.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <ListagemProdutos />
 </template>
+
+<style scoped></style>
